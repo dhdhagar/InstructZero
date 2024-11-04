5a7673c6e9687ecd48fbb493f6e22c3dc8b91eaa
Dhruv Agarwal <dhruv.agarwal17@gmail.com>
2024-11-03 19:11:52 -0500

    Fix

--- InstructZero/experiments/run_semantle.py
+++ InstructZero/experiments/run_semantle.py
@@ -203,7 +203,7 @@ e.g. {{\"response\": [\"word1\", \"word2\",...]}})"""
             {"role": "user", "content": user}
         ]
         prompt_templatized = self.tokenizer.apply_chat_template(prompt, tokenize=False, add_generation_prompt=True)
-        prompt_templatized_tkns = self.tokenizer.apply_chat_template(prompt, tokenize=False, add_generation_prompt=True)
+        prompt_templatized_tkns = self.tokenizer.apply_chat_template(prompt, tokenize=True, add_generation_prompt=True)
 
         return prompt_templatized, prompt_templatized_tkns
 
